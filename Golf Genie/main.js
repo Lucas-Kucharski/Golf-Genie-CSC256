5e23e172185e6deda28835e3b188194b7cb3346d
Lucas-Kucharski <77312379+Lucas-Kucharski@users.noreply.github.com>
2023-06-08 01:08:10 -0700

    Add files via upload

---
+++ Golf Genie/main.js
@@ -0,0 +1,28 @@
+var userInfo = document.forms
+
+
+//This function takes the userInfo variables and if it is equal to the array then the window alert will fire.
+function diplayClubs() {
+    if (userInfo = ["1", "1", "1", "1"]) {
+        window.alert("Titleist T100's are the clubs for you!");
+    }
+
+    else if (userInfo = ["2", "2", "2", "2"]) {
+        window.alert("Callaway Maverik Irons are the clubs for you!")
+    }
+
+    else if (userInfo = ["3", "3", "3", "3"]) {
+        window.alert("TaylorMade Stealth Irons are the clubs for you!")
+    }
+
+    else if (userInfo = ["4", "4", "4", "4"]) {
+        window.alert("Mizuno JPX 923 Irons are the clubs for you!")
+    }
+
+}
+
+
+
+
+
+
